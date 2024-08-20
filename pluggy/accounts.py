dc9c1fcb087df6ada7394075c66fe50114dab242
vini-machado <viniciuscmachado22@gmail.com>
2024-08-20 13:32:23 -0300

    changing api methods

--- pluggy/accounts.py
+++ pluggy/accounts.py
@@ -1,21 +1,17 @@
+from typing import Optional
 from pluggy.api import PluggyAPI
 
 
 class AccountsAPI(PluggyAPI):
-    BASE_ENDPOINT = 'accounts'
+    BASE_ENDPOINT = "accounts"
 
     def __init__(self) -> None:
         super().__init__()
 
-    def list_accounts(self, **kwargs):
-        return self.call(
-            endpoint = self.BASE_ENDPOINT,
-            query_params = kwargs
-        )
+    def list(self, item_id: str, type: Optional[str] = None):
+        query_params = {"itemId": item_id, "type": type}
+        return self.call(endpoint=self.BASE_ENDPOINT, query_params=query_params)
 
-    def retrieve(self, account_id: int, **kwargs):
-        endpoint = f'{self.BASE_ENDPOINT}/{account_id}'
-        return self.call(
-            endpoint = endpoint,
-            query_params = kwargs
-        )
+    def retrieve(self, account_id: int):
+        endpoint = f"{self.BASE_ENDPOINT}/{account_id}"
+        return self.call(endpoint=endpoint)
--- pluggy/connectors.py
+++ pluggy/connectors.py
@@ -1,29 +1,42 @@
+from typing import Optional
 from pluggy.api import PluggyAPI
 
 
 class ConnectorsAPI(PluggyAPI):
-    BASE_ENDPOINT = 'connectors'
+    BASE_ENDPOINT = "connectors"
 
     def __init__(self) -> None:
         super().__init__()
 
-    def list_connectors(self, **kwargs):
-        return self.call(
-            endpoint = self.BASE_ENDPOINT,
-            query_params = kwargs
-        )
+    def list(
+        self,
+        countries: Optional[list[str]] = None,
+        types: Optional[list[str]] = None,
+        name: Optional[str] = None,
+        sandbox: Optional[bool] = None,
+        health_details: Optional[bool] = None,
+        is_open_finance: Optional[bool] = None,
+        supports_payment_initiation: Optional[bool] = None,
+    ):
+        query_params = {
+            "countries": countries,
+            "types": types,
+            "name": name,
+            "sandbox": sandbox,
+            "healthDetails": health_details,
+            "isOpenFinance": is_open_finance,
+            "supportsPaymentInitiation": supports_payment_initiation,
+        }
+        return self.call(endpoint=self.BASE_ENDPOINT, query_params=query_params)
 
-    def retrieve(self, connector_id: int, **kwargs):
-        endpoint = f'{self.BASE_ENDPOINT}/{connector_id}'
-        return self.call(
-            endpoint = endpoint,
-            query_params = kwargs
-        )
-    
+    def retrieve(self, connector_id: int, health_details: Optional[bool] = None):
+        endpoint = f"{self.BASE_ENDPOINT}/{connector_id}"
+
+        query_params = {
+            "healthDetails": health_details,
+        }
+        return self.call(endpoint=endpoint, query_params=query_params)
+
     def validate(self, connector_id: int, **kwargs):
-        endpoint = f'{self.BASE_ENDPOINT}/{connector_id}/validate'
-        return self.call(
-            endpoint = endpoint,
-            method='POST',
-            query_params = kwargs
-        )
+        endpoint = f"{self.BASE_ENDPOINT}/{connector_id}/validate"
+        return self.call(endpoint=endpoint, method="POST", query_params=kwargs)
